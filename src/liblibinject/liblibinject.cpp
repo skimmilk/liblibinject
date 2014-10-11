271c70a09345089f0c90c30216641d12d7d0303f
William DeGraaf <nswilliwa@gmail.com>
2014-10-11 12:59:37 -0500

    Separate multiarch declarations into separate file

--- src/liblibinject/liblibinject.cpp
+++ src/liblibinject/liblibinject.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <functional>
 
 // c includes
 #include <sys/ptrace.h>
@@ -24,6 +25,7 @@
 
 // project includes
 #include "liblibinject.h"
+#include "multiarch.h"
 
 
 namespace inject {
@@ -37,66 +39,6 @@ namespace inject {
 //   another region of memory that we can play with
 // We inject the code we want in this region of memory
 // We restore the small region of memory to normal
-
-#define ATOI(m) #m
-#define PCHECK(a,b,c,d) {if (ptrace(a,b,c,d)) error(1, errno, __FILE__ ":%d", __LINE__);}
-
-// length of the executable buffer in the attached application
-#define MAP_LENGTH (sysconf(_SC_PAGESIZE))
-
-// Stores information about the attached application
-struct remote_state
-{
-	int verbose;
-	pid_t pid;
-	user_regs_struct regs_old;
-	// Where we mapped the region to inject code
-	long executable_page;
-	std::vector<long> stack_backup;
-};
-
-#ifdef __x86_64__
-// AMD64 low-level helpers
-// Pointer to the stack frame
-#define FRAME_PTR(m) m.rbp
-#define STACK_TOP(m) m.rsp
-// Pointer to the current execution location
-#define COUNTER(m) m.rip
-#define INJECT_SYSCALL asm volatile (".align 8\t\nsyscall");
-#define RESULT(m) m.rax
-// Sets the arguments to a system call
-void set_syscall_arguments(const remote_state& state, long syscall_n,
-		long a1, long a2=0, long a3=0, long a4=0, long a5=0, long a6=0)
-{
-	user_regs_struct newregs;
-	PCHECK(PTRACE_GETREGS, state.pid, 0, &newregs);
-
-	// x86_64 rdi rsi rdx r10 r8 r9
-	newregs.rdi = a1;
-	newregs.rsi = a2;
-	newregs.rdx = a3;
-	newregs.r10 = a4;
-	newregs.r8 = a5;
-	newregs.r9 = a6;
-
-	// The RAX register holds the syscall number
-	newregs.rax = newregs.orig_rax = syscall_n;
-
-	// Set the registers
-	PCHECK(PTRACE_SETREGS, state.pid, 0, &newregs);
-}
-#else
-// Intel32 low-level helpers
-#define FRAME_PTR(m) m.ebp
-#define STACK_TOP(m) m.esp
-#define COUNTER(m) m.eip
-#define INJECT_SYSCALL asm volatile (".align 4\t\nint 0x80");
-#define RESULT(m) m.eax
-void set_syscall_arguments(remote_state state, long syscall_n
-		long a1, long a2=0, long a3=0, long a4=0, long a5=0, long a6=0)
-{}
-#endif
-
 
 
 // Returns the location of libname in the attached application
@@ -161,7 +103,8 @@ long make_mmap_call(const remote_state& state, long exec_base)
 
 	inject_syscall(state, exec_base);
 
-	// Now the program is stuck right before the system call, make it call mmap
+	// Now the program is stuck right before the system call
+	// Make it call mmap
 	//mmap(void *addr, size_t length, int prot, int flags,
 	//                  int fd, off_t offset);
 	set_syscall_arguments(state, SYS_mmap,
@@ -169,7 +112,7 @@ long make_mmap_call(const remote_state& state, long exec_base)
 			MAP_ANONYMOUS | MAP_PRIVATE, 0);
     PCHECK(PTRACE_GETREGS, state.pid, 0, &regs);
 
-	// Make it call the syscall
+	// Make it call the correct syscall
 	PCHECK(PTRACE_SYSCALL, state.pid, 0, 0);
 	wait(0);
     PCHECK(PTRACE_GETREGS, state.pid, 0, &regs);
@@ -186,6 +129,16 @@ long make_mmap_call(const remote_state& state, long exec_base)
     PCHECK(PTRACE_POKEDATA, state.pid, exec_base, backup);
 
 	return RESULT(regs);
+}
+
+// This function is injected into the program
+void external_call_dlopen(
+		std::function<void*(const char*,int)> extern_dlopen,
+		std::function<int(int)> extern_syscall,
+		const char* extern_filename)
+{
+	extern_dlopen(extern_filename, 0);
+	extern_syscall(1337);
 }
 
 inject_error create_remote_thread(pid_t pid, int verbose)
@@ -205,13 +158,23 @@ inject_error create_remote_thread(pid_t pid, int verbose)
     // Backup the registers
     PCHECK(PTRACE_GETREGS, pid, 0, &state.regs_old);
 
-    // Get the location of libc in the attached program and in the current one
-    state.executable_page = make_mmap_call(state, baseof(pid, "libc"));
+    // Get the base of libc
+    auto extern_libc_base = baseof(pid, "libc");
+    auto libc_base = baseof(getpid(), "libc");
+    // Get the offset of the functions __libc_dlopen_mode and syscall
+    //   in the process
+    auto extern_dlopen = __libc_dlopen_mode - libc_base + extern_libc_base;
+    auto extern_syscall = syscall - libc_base + extern_libc_base;
+
     if (verbose)
-    {
+    	printf(stderr, "External dlopen is at %p\nExternal syscall is at %p\n",
+    			extern_dlopen, extern_syscall);
+
+    // Get the location of libc in the attached program and in the current one
+    state.executable_page = make_mmap_call(state, extern_libc_base);
+    if (verbose)
     	fprintf(stderr, "Executable page is at %p, or error %s\n",
     			(void*)state.executable_page, strerror(state.executable_page * -1));
-    }
 
     // Restore the registers from the backup
 	PCHECK(PTRACE_SETREGS, state.pid, 0, &state.regs_old);
---
+++ src/liblibinject/multiarch.h
@@ -0,0 +1,80 @@
+/*
+ * multiarch.h
+ *
+ *  Created on: Oct 10, 2014
+ *      Author: skim
+ */
+
+#ifndef MULTIARCH_H_
+#define MULTIARCH_H_
+
+
+namespace inject{
+
+
+// Stores information about the attached application
+struct remote_state
+{
+	int verbose;
+	// PID of the application to mess with
+	pid_t pid;
+	// The backup registers of the program
+	user_regs_struct regs_old;
+	// Where we mapped the region to inject code
+	long executable_page;
+	// The backup of the stack
+	std::vector<long> stack_backup;
+};
+
+
+#define PCHECK(a,b,c,d) {if (ptrace(a,b,c,d)) error(1, errno, __FILE__ ":%d", __LINE__);}
+
+// length of the executable buffer in the attached application
+#define MAP_LENGTH (sysconf(_SC_PAGESIZE))
+
+
+#ifdef __x86_64__
+// AMD64 low-level helpers
+// Pointer to the stack frame
+#define FRAME_PTR(m) m.rbp
+#define STACK_TOP(m) m.rsp
+// Pointer to the current execution location
+#define COUNTER(m) m.rip
+#define INJECT_SYSCALL asm volatile (".align 8\t\nsyscall");
+#define RESULT(m) m.rax
+// Sets the arguments to a system call
+void set_syscall_arguments(const remote_state& state, long syscall_n,
+		long a1, long a2=0, long a3=0, long a4=0, long a5=0, long a6=0)
+{
+	user_regs_struct newregs;
+	PCHECK(PTRACE_GETREGS, state.pid, 0, &newregs);
+
+	// x86_64 rdi rsi rdx r10 r8 r9
+	newregs.rdi = a1;
+	newregs.rsi = a2;
+	newregs.rdx = a3;
+	newregs.r10 = a4;
+	newregs.r8 = a5;
+	newregs.r9 = a6;
+
+	// The RAX register holds the syscall number
+	newregs.rax = newregs.orig_rax = syscall_n;
+
+	// Set the registers
+	PCHECK(PTRACE_SETREGS, state.pid, 0, &newregs);
+}
+#else
+// Intel32 low-level helpers
+#define FRAME_PTR(m) m.ebp
+#define STACK_TOP(m) m.esp
+#define COUNTER(m) m.eip
+#define INJECT_SYSCALL asm volatile (".align 4\t\nint 0x80");
+#define RESULT(m) m.eax
+void set_syscall_arguments(remote_state state, long syscall_n
+		long a1, long a2=0, long a3=0, long a4=0, long a5=0, long a6=0)
+{}
+#endif
+
+} /* namespace inject */
+
+#endif /* MULTIARCH_H_ */
